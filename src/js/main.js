21f8eb4b820d240fe6205b998fffebdb15f3282d
JessicaVR86 <jessica.vld.rdi@gmail.com>
2025-04-30 20:05:29 +0200

    apply js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const btnShow1 = document.querySelector('.js_btnshow1');
+const btnShow2 = document.querySelector('.js_btnshow2');
+const btnShow3 = document.querySelector('.js_btnshow3');
+
+const jsformMain = document.querySelector('.js_form_main');
+
+const jsform1 = document.querySelector('.js_form1');
+const jsform2 = document.querySelector('.js_form2');
+
+btnShow1.addEventListener('click' , () =>{
+    jsform1.innerHTML = jsform1.innerHTML  + `
+    <form action="" method="post">
+      <fieldset class="form">
+        <legend>About you</legend>
+
+        <label for="insert_photo">Add your photo:</label>
+        <input id="insert_photo" type="file" name="insert_photo" />
+
+        <label for="email">Email: </label>
+        <input id="email" type="email" />
+
+        <label for="name">Name:</label>
+        <input id="name" type="text" name="name" />
+
+        <label for="age">Age:</label>
+        <input id="age" type="text" name="age" />
+
+        <label for="quality">One quality that defines you.</label>
+        <input id="quality" type="text" name="quality" />
+      </fieldset>
+    </form>
+`;
+
+} );
