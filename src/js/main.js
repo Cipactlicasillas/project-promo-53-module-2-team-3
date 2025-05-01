5ea70ef9314e372661e41ccde15e0802e7528869
JessicaVR86 <jessica.vld.rdi@gmail.com>
2025-05-01 16:41:25 +0200

    add js

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,67 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+console.log(">> Ready :)");
 
-const btnShow1 = document.querySelector('.js_btnshow1');
-const btnShow2 = document.querySelector('.js_btnshow2');
-const btnShow3 = document.querySelector('.js_btnshow3');
+const btnShow1 = document.querySelector(".js_btnshow1");
+const btnShow2 = document.querySelector(".js_btnshow2");
+const btnShow3 = document.querySelector(".js_btnshow3");
 
-const jsformMain = document.querySelector('.js_form_main');
+const jsformMain = document.querySelector(".js_form_main");
 
-const jsform1 = document.querySelector('.js_form1');
-const jsform2 = document.querySelector('.js_form2');
+const jsform1 = document.querySelector(".js_form1");
+const jsform2 = document.querySelector(".js_form2");
 
-btnShow1.addEventListener('click' , () =>{
-    jsform1.innerHTML = jsform1.innerHTML  + `
-    <form action="" method="post">
-      <fieldset class="form">
-        <legend>About you</legend>
+btnShow1.addEventListener("click", () => {
+  const contentForm1 = `<form action="" method="post">
+    <fieldset class="form">
+      <legend>About you</legend>
 
-        <label for="insert_photo">Add your photo:</label>
-        <input id="insert_photo" type="file" name="insert_photo" />
+      <label for="insert_photo">Add your photo:</label>
+      <input id="insert_photo" type="file" name="insert_photo" />
 
-        <label for="email">Email: </label>
-        <input id="email" type="email" />
+      <label for="email">Email: </label>
+      <input id="email" type="email" />
 
-        <label for="name">Name:</label>
-        <input id="name" type="text" name="name" />
+      <label for="name">Name:</label>
+      <input id="name" type="text" name="name" />
 
-        <label for="age">Age:</label>
-        <input id="age" type="text" name="age" />
+      <label for="age">Age:</label>
+      <input id="age" type="text" name="age" />
 
-        <label for="quality">One quality that defines you.</label>
-        <input id="quality" type="text" name="quality" />
-      </fieldset>
-    </form>
-`;
+      <label for="quality">One quality that defines you.</label>
+      <input id="quality" type="text" name="quality" />
+    </fieldset>
+  </form>`;
+  jsform1.innerHTML = contentForm1;
+});
 
-} );
+btnShow2.addEventListener('click' , () => {
+const contentForm2 =`<fieldset class="form">
+      <legend>What place do you prefer?</legend>
+      <div class="place1">
+        <input type="radio" name="place" id="beach" value="p1" />
+        <label for="beach">Beach</label>
+        <input type="radio" name="place" id="mountain" value="p2" />
+        <label for="mountain">Mountain</label>
+      </div>
+    </fieldset>
+    <fieldset class="form">
+      <legend>What drink do you prefer?</legend>
+      <div class="drink1">
+        <input type="radio" name="drink" id="beer" value="p1" />
+        <label for="beer">Beer</label>
+        <input type="radio" name="drink" id="wine" value="p2" />
+        <label for="wine">Wine</label>
+      </div>
+    </fieldset>
+    <fieldset class="form">
+      <legend>What food do you prefer?</legend>
+      <div class="food1">
+        <input type="radio" name="food" id="meat" value="p1" />
+        <label for="meat">Meat</label>
+        <input type="radio" name="food" id="vegan" value="p2" />
+        <label for="vegan"> Vegan</label>
+      </div>
+    </fieldset>`;
+    jsform2.innerHTML = contentForm2;
+})
